917b35724a915e228ec697f9d17da24234971f78
minchodang <minsu910725@gmail.com>
2024-01-19 21:23:14 +0900

    Feat: signup-php, connnection-php

---
+++ user/signup.php
@@ -0,0 +1,12 @@
+<?php
+include '../connection.php';
+
+$userName = $_POST['user_name'];
+$userEmail = $_POST['user_email'];
+$userPassword = md5($_POST['user_password']);
+
+
+
+$sqlQuery = "INSERT INTO user_table SET user_name = '$userName', user_email='$userEmail', user_password='$userPassword'";
+
+$resultQuery = $connection ->  query($sqlQuery);
